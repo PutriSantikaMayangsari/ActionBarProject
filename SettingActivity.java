85e4fc6a2aaee4b0c65b7aeb4100e0d62aced049
PutriSantikaMayangsari <83493403+PutriSantikaMayangsari@users.noreply.github.com>
2022-04-10 20:47:21 +0700

    Create SettingActivity.java

---
+++ SettingActivity.java
@@ -0,0 +1,17 @@
+package com.example.myviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
